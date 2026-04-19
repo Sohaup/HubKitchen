c2af091a9853c25478ea3d0f627856babcaf2a3f
Sohaup <sohybta560@gmail.com>
2026-04-19 13:57:16 +0200

    module HR initail commit

--- src/modules/auth/app/http/routes/authRoutes.php
+++ src/modules/auth/app/http/routes/authRoutes.php
@@ -6,7 +6,6 @@ use PostApi\modules\auth\app\http\middlewares\GateMiddleware;
 use PostApi\modules\auth\helpers\types\RoleTypes;
 use PostApi\shared\app\http\proxies\ProxyMiddlewareForRoute;
 use PostApi\shared\app\http\routes\Route\Route;
-use PostApi\shared\app\http\routes\Route\RouteCollection;
 use PostApi\shared\app\http\types\HttpMethodsType;
 use PostApi\shared\helpers\fecade\Urls;
 
